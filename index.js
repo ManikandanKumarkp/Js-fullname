3e9a86451da4300ab563b1115fcfb5b217f2b55d
ManikandanKumarkp <manikandankumarkp@gmail.com>
2022-04-07 13:04:54 +0530

    2 files modified

--- index.js
+++ index.js
@@ -6,12 +6,13 @@ const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>
 <div>
 <h5>Manikandank</h5>
+
 </div>`;
 
 const person ={ fname:"mani", lname:"kandan"
 };
 
-document.getElementById("demo").innerHTML = person.fname +"" +person.lname;
+document.getElementById("").innerHTML = person.fname +"" +person.lname;
 
 const cars = ["Saab", "Volvo", "BMW"];
 document.getElementById("demo").innerHTML = cars;
